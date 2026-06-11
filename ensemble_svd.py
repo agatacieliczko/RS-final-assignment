75d5b70081d0a301e3c8f19cf34f4312c338dfc9
Patricija Dziuzaite <116151182+dziuzaite@users.noreply.github.com>
2026-06-11 23:09:55 +0200

    change data paths for Kaggle compatibility

--- ensemble_svd.py
+++ ensemble_svd.py
@@ -13,8 +13,8 @@ from train import InferenceDataset
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
-PROCESSED_DIR = Path("processed")
-DATA_DIR = Path("data")
+PROCESSED_DIR = Path("/kaggle/working/RS-final-assignment/processed")
+DATA_DIR = Path("/kaggle/working/RS-final-assignment/data")
 CHECKPOINT = Path("/kaggle/working/runs/exp/best_model.pt")OUT_PATH = Path("runs/exp/submission_ensemble_svd.csv")
 
 SVD_COMPONENTS = 128
